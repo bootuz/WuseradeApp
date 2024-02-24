557fc4edba45346284f143f95d5330eff8b8f5c3
Astemir Boziev <bootuz07@gmail.com>
2024-02-24 19:06:13 +0400

    ready to release

---
+++ wuserade/Managers/AnalyticsManager.swift
@@ -0,0 +1,8 @@
+//
+//  AnalyticsManager.swift
+//  Wuserade
+//
+//  Created by Астемир Бозиев on 24.02.2024.
+//
+
+import Foundation
---
+++ wuserade/Views/Poems/PoemCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  PoemCategoryView.swift
+//  Wuserade
+//
+//  Created by Астемир Бозиев on 24.02.2024.
+//
+
+import SwiftUI
+
+struct PoemCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PoemCategoryView()
+}
