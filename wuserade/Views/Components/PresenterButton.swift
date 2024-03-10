93ff6ccf9cc2133d13947b1367258b99353c65d3
Astemir Boziev <bootuz07@gmail.com>
2024-03-10 14:51:40 +0400

    new component PresenterButton

--- wuserade/Views/Components/PresenterButton.swift
+++ wuserade/Views/Components/PresenterButton.swift
@@ -7,28 +7,71 @@
 
 import SwiftUI
 
-struct PresenterButton<Destination: View, Label: View>: View {
-    let destination: () -> Destination
-    let label: () -> Label
+struct PresenterButton<Label, Destination>: View where Label : View, Destination : View {
+
+    enum PresentationType {
+        case sheet
+        case fullScreen
+        case popover
+    }
+
+    let destination: Destination
+    let label: Label
+    let presentationType: PresentationType
 
     @State private var showDestinationView: Bool = false
-    @AppStorage("colorScheme") private var colorScheme: Bool = false
+    @Environment(\.colorScheme) private var colorScheme
+
+    init(presentationType: PresentationType, @ViewBuilder destination: () -> Destination, @ViewBuilder label: () -> Label) {
+        self.presentationType = presentationType
+        self.destination = destination()
+        self.label = label()
+    }
 
     var body: some View {
         Button {
             showDestinationView.toggle()
         } label: {
-            label()
+            label
         }
-        .sheet(isPresented: $showDestinationView, content: {
-            destination()
-                .preferredColorScheme(colorScheme ? .dark : .light)
+        .sheet(isPresented: Binding(
+            get: { showDestinationView && presentationType == .sheet },
+            set: { newValue in
+                    if !newValue {
+                        showDestinationView = false
+                    }
+                }
+            ), content: {
+                destination
+                    .preferredColorScheme(colorScheme)
+        })
+        .fullScreenCover(isPresented: Binding(
+            get: { showDestinationView && presentationType == .fullScreen },
+            set: { newValue in
+                if !newValue {
+                    showDestinationView = false
+                }
+            }
+        ), content: {
+            destination
+                .preferredColorScheme(colorScheme)
+        })
+        .popover(isPresented: Binding(
+            get: { showDestinationView && presentationType == .popover },
+            set: { newValue in
+                if !newValue {
+                    showDestinationView = false
+                }
+            }
+        ), content: {
+            destination
+                .preferredColorScheme(colorScheme)
         })
     }
 }
 
 #Preview {
-    PresenterButton(destination: {
+    PresenterButton(presentationType: .fullScreen, destination: {
         SettingsView()
     }, label: {
         Image(systemName: "gearshape")
