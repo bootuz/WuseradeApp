992f9f1ff5ef2f341bd3d670143cddb70aa6be0b
Astemir Boziev <bootuz07@gmail.com>
2024-03-10 14:51:20 +0400

    notifications

--- wuserade/wuseradeApp.swift
+++ wuserade/wuseradeApp.swift
@@ -8,15 +8,37 @@
 import SwiftUI
 import SwiftData
 import FirebaseCore
+import FirebaseMessaging
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        application.registerForRemoteNotifications()
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let fcm = Messaging.messaging().fcmToken {
+            print("fcm", fcm)
+        }
+    }
+}
 
 @main
 struct wuseradeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     @AppStorage("firstLaunch") private var firstLaunch = true
     @StateObject private var fontSettingsManager = FontSettingsManager()
-
-    init() {
-        FirebaseApp.configure()
-    }
+    @StateObject private var notificationManager = NotificationManager()
 
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
@@ -34,10 +56,14 @@ struct wuseradeApp: App {
 
     var body: some Scene {
         WindowGroup {
-            TabBarView()
+            TabBarView(notificationManager: notificationManager)
                 .environmentObject(fontSettingsManager)
                 .tint(.primary)
-                .sheet(isPresented: $firstLaunch, content: {
+                .sheet(isPresented: $firstLaunch, onDismiss: {
+                    Task {
+                        await notificationManager.request()
+                    }
+                }, content: {
                     OnboardingView()
                         .interactiveDismissDisabled()
                 })
